2d8de775ce6f8907267dd85f9a2b866ddc4bf5f5
Gabriela Nairac <gabrielanairac@gmail.com>
2024-03-18 19:38:33 -0300

    carro

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//PRIMERA ENTREGA
+
 //ALGORITMO DE FORMULARIO DE INGRESO DE UN NUEVO CLIENTE
 
 // Almacenar el número de cliente
@@ -94,8 +96,108 @@ alert(saludar(nombre, apellido));
 // Incrementar el número de cliente para el próximo nuevo cliente 
 incrementarNumeroCliente();
 
+    //SEGUNDA ENTREGA
+// Función CONSTRUCTOR para crear la ficha de producto 
+function Producto(SKU, Imagen, Categoria, Nombre, Descripcion, Color, Precio, Descuento) {
+    this.SKU = SKU; // Stock Keeping Unit, id único de producto
+    this.Imagen = Imagen;
+    this.Categoria = Categoria;
+    this.Nombre = Nombre;
+    this.Descripcion = Descripcion;
+    this.Color = Color;
+    this.Precio = parseFloat(Precio); // Float para que admita decimales
+    this.Descuento = parseFloat(Descuento); // Float para que admita decimales
+}
 
+//Creación del catálogo de productos 
+//Agregar valores a las propiedades para instanciar el objeto Producto
+const producto1 = new Producto(
+    "21PEWT2150S",
+    "https://imagizer.imageshack.com/img924/1756/vNv6W4.png", //img localizada en un servidor externo
+    "Línea Blanca",
+    "Aire Acondicionado LG",
+    "CAPACIDAD: 12,000 BTU · DIMENSIONES LXAXP: Evaporador 837 x 308 x 189 mm",
+    "Blanco",
+    "15000.99",
+    "10.00"
+);
 
+const producto2 = new Producto(
+    "18P3WT1865A",
+    "https://imagizer.imageshack.com/img922/7452/JKBxHt.png", //img localizada en un servidor externo
+    "Línea Blanca",
+    "Heladera SAMSUNG No frost 363 Lts",
+    "CAPACIDAD: 363 litros. Freezer superior. Iluminación interior.",
+    "Blanco",
+    "25000.00",
+    "00.00"
+);
 
+const producto3 = new Producto(
+    "14P3WT1431A",
+    "https://imagizer.imageshack.com/img924/7990/UOQ2gC.png", //img localizada en un servidor externo
+    "Línea Blanca",
+    "Minibar HACEB 95 9002547",
+    "Frost Congelador Inferior. NÚMERO DE PUERTAS: 1. Dispensador de Hielo.",
+    "Gris acero",
+    "35000.00",
+    "00.00"
+);
+
+// Función para calcular el precio final multiplicando por 1.21 y antes verficar si tiene dto
+function precioFinal(producto) {
+    const precioSinIva = producto.Precio;
+    const descuento = producto.Descuento;
+    if (descuento > 0) // Verificar si el producto tiene descuento > 0
+    {
+        const precioPromocional = precioSinIva - (precioSinIva * (descuento / 100)); // Descontar el descuento al precio sin IVA
+        return precioPromocional;
+    } else {
+        return precioSinIva;
+    }
+}
+
+// Carro de compras
+const carroDeCompras = [];
+
+// Estando el usuario en la ficha del producto, se le pregunta si quiere agregarlo al carrito 
+const respuestaInicioCompra = prompt("¿Deseas agregar productos al carro de compras? SI o NO");
+
+// Opciones según la respuesta
+if (respuestaInicioCompra.toUpperCase() === "SI") { // Transformar a mayúsculas para unificar las respuestas
+    carroDeCompras.push(producto1); // Agregar producto al carro
+    console.log("Producto agregado"); // Mensaje de confirmación
+    const respuestaContinuarCompra = prompt("¿Deseas agregar otro producto al carro de compras? SI o NO");
+    if (respuestaContinuarCompra.toUpperCase() === "SI") { // Unificar las respuestas
+        carroDeCompras.push(producto2); // Agregar producto2 al carro;
+        console.log("Producto agregado"); // Mensaje de confirmación
+    }
+} else { 
+    const siguientePaso = prompt("¿Qué deseas hacer? Guardar en Lista de Favoritos o Excluir producto");
+    if (siguientePaso.toUpperCase() === "GUARDAR EN LISTA DE FAVORITOS") // Unificar las respuestas
+    {
+        console.log("Producto guardado en Lista de Favoritos");
+    } else if (siguientePaso.toUpperCase() === "EXCLUIR PRODUCTO") // Unificar las respuestas
+    {
+        const productoAEliminar = prompt("Selecciona el producto a excluir");
+        const indexProductoAEliminar = carroDeCompras.findIndex(producto => producto.Nombre === productoAEliminar); //Busca la posición dentro del array del carro a través de la propiedad Nombre
+        if (indexProductoAEliminar !== -1) //Verificar que el valor exista siendo distinto a -1
+        {
+            carroDeCompras.splice(indexProductoAEliminar, 1); // Eliminar el producto con slice
+            console.log("Producto excluido correctamente");
+        }
+    }
+}
+
+// Aplicar la función precioFinal a cada producto en carroDeCompras
+carroDeCompras.forEach(producto => { // Función FLECHA
+    producto.Precio = precioFinal(producto);
+});
+
+console.log(carroDeCompras);
+
+// Array de los nombres de los productos confirmados en la compra
+const nombresProductosComprados = carroDeCompras.map(producto => producto.Nombre).join(", ");
+console.log("Tu compra incluye: " + nombresProductosComprados);
 
 
